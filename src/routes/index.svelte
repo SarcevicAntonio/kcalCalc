cc1c7dbf5b2c24fb71955fd482d4a42f3d0ef127
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-04-24 18:49:22 +0200

    removed useless content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-
 	import { toISODateString } from '$lib/dateHelpers';
 	import { user } from '$lib/stores/user';
 	import { onMount } from 'svelte';
-
-	import IconHome from '~icons/mdi/home';
-	import IconLogin from '~icons/mdi/login-variant';
 
 	onMount(() => {
 		if ($user) {
