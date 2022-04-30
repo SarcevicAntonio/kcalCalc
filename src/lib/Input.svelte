a26b791009c05e05cf15be96ef6f5f91aca917b8
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-05-01 00:14:04 +0200

    clean input

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import Fuse from 'fuse.js';
 	import { evaluate } from 'mathjs';
 	import { createEventDispatcher } from 'svelte';
 	import { v4 as uuidv4 } from 'uuid';
-	import clickOutside from './clickOutside';
 
 	const id = uuidv4();
 	const dispatch = createEventDispatcher();
@@ -17,21 +15,6 @@
 	export let disabled = false;
 
 	export let outlined = false;
-
-	// autocomplete
-	export let options = undefined;
-	export let optionLabel = 'label';
-	let showOptions = false;
-	let filtered = options;
-
-	$: setFiltered(options, value);
-	function setFiltered(options: any, value: string | number) {
-		if (!options || !(value + '').trim()) return;
-		const fuse = new Fuse(options, {
-			keys: [optionLabel],
-		});
-		filtered = fuse.search(value + '').map((e) => e.item);
-	}
 
 	const handleInput = (e: Event) => {
 		// handle types
@@ -62,7 +45,6 @@
 
 <div
 	on:click={() => inputElement.focus()}
-	use:clickOutside={{ enabled: showOptions, cb: () => (showOptions = false) }}
 	class:error={canNotEvaluate}
 	class:disabled
 	class:outlined
@@ -83,31 +65,10 @@
 		on:input={handleInput}
 		on:change
 		on:focus={(e) => {
-			showOptions = true;
 			dispatch('focus', e);
 		}}
 		on:blur={handleBlur}
 	/>
-	<!-- Autocomplete -->
-	{#if showOptions && filtered?.length && value}
-		<div class="options">
-			{#each filtered as option}
-				<button
-					disabled={option.disabled}
-					on:click={() => {
-						value = option[optionLabel];
-						showOptions = false;
-						dispatch('input', value);
-						dispatch('change', value);
-						dispatch('select', option);
-					}}
-				>
-					{option[optionLabel]}
-					<slot name="option-button-extra" {option} />
-				</button>
-			{/each}
-		</div>
-	{/if}
 </div>
 
 <style lang="postcss">
@@ -136,8 +97,6 @@
 		}
 
 		&:focus-within {
-			/* outline: 5px auto Highlight;
-			outline: 5px auto -webkit-focus-ring-color; */
 			border-color: var(--md-primary);
 		}
 	}
@@ -160,30 +119,13 @@
 		}
 	}
 
-	.options {
-		position: absolute;
-		top: 3.5em;
-		z-index: 999;
-		left: 0;
-		right: 0;
-		background-color: var(--md-surface);
-		padding: 0.25em;
-		border-radius: 28px;
-		max-height: 180px;
-		overflow-y: auto;
-	}
-	button {
-		text-align: unset;
-		border: none;
-		background: var(--md-secondary-container);
-		color: var(--md-on-secondary-container);
-	}
 	.error {
 		border-color: var(--md-error);
 		& label {
 			color: var(--md-error);
 		}
 	}
+
 	.disabled {
 		background-color: var(--md-on-secondary-container--opacity-008);
 		color: var(--md-on-surface);
