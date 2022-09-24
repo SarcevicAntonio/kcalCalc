7aaa1c507ab3e16ba71be9e69ef960a05b526cdb
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-09-24 16:08:40 +0200

    barcode adjustments?

--- src/lib/components/BarCodeScanDialog.svelte
+++ src/lib/components/BarCodeScanDialog.svelte
@@ -7,24 +7,22 @@
 	import { Dialog } from 'as-comps';
 	import { createEventDispatcher } from 'svelte';
 	import IcRoundBarcode from '~icons/ic/round-barcode';
+	import IcRoundQrCodeScanner from '~icons/ic/round-qr-code-scanner';
 	const dispatch = createEventDispatcher();
 
 	let isOpen = false;
-
-	let codeReader: BrowserMultiFormatOneDReader;
+	let codeReader = new BrowserMultiFormatOneDReader();
 	let videoInputDevices: MediaDeviceInfo[];
 	let selectedDeviceId: string;
 	let previewElem: HTMLVideoElement;
 	let controls: IScannerControls;
 
 	async function startScanner() {
-		codeReader = new BrowserMultiFormatOneDReader();
+		closeScanner();
 		videoInputDevices = await BrowserCodeReader.listVideoInputDevices();
 		selectedDeviceId = videoInputDevices[0].deviceId;
-		console.log(`Started decode from camera with id ${selectedDeviceId}`);
 		controls = await codeReader.decodeFromVideoDevice(selectedDeviceId, previewElem, (result) => {
 			if (!result) return;
-			console.log('RESULT: ', result, typeof result);
 			dispatch('scanned', result.getText());
 			closeScanner();
 			isOpen = false;
@@ -32,16 +30,54 @@
 	}
 
 	function closeScanner() {
-		if (!controls) return;
-		controls.stop();
+		controls?.stop();
+		controls = undefined;
 	}
 </script>
 
-<Dialog triggerClass="btn tonal" on:introend={startScanner} on:dismiss={closeScanner} bind:isOpen>
+<Dialog
+	noCloseButton
+	triggerClass="inline-btn tonal"
+	on:introend={startScanner}
+	on:dismiss={closeScanner}
+	bind:isOpen
+>
 	<svelte:fragment slot="trigger-label">
 		<IcRoundBarcode />
 	</svelte:fragment>
-	<video bind:this={previewElem}>
+	{#if !controls}
+		<div class="init">
+			<IcRoundQrCodeScanner />
+			<span>Initializing Scanner...</span>
+		</div>
+	{/if}
+	<video
+		bind:this={previewElem}
+		height={!controls ? 0 : undefined}
+		width={!controls ? 0 : undefined}
+	>
 		<track kind="captions" />
 	</video>
 </Dialog>
+
+<style>
+	video {
+		max-width: 100%;
+		max-height: 100%;
+		border-radius: 1000000;
+	}
+
+	.init {
+		color: var(--md-on-surface);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 0.25em;
+		font-size: 3rem;
+	}
+
+	.init span {
+		font-size: 1rem;
+	}
+</style>
