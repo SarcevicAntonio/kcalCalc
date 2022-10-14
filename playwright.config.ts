de267e3468d2b85c4553d6a5579d21e924bbc0b6
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-10-14 16:11:04 +0200

    add back html reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,11 @@ const config: PlaywrightTestConfig = {
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: [['list'], ['junit', { outputFile: 'reports/playwright.junit.xml' }]],
+	reporter: [
+		['list'],
+		['html', { open: 'never', outputFolder: 'reports' }],
+		['junit', { outputFile: 'reports/playwright.junit.xml' }],
+	],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
