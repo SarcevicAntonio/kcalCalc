25b39a85541588dd1134b6a9180f4f0b3c20d35f
Antonio Sarcevic <sarcevicantonio@gmail.com>
2026-04-03 14:54:16 +0200

    deprecate compoent 💩

---
+++ src/lib/components/Deprecation.svelte
@@ -0,0 +1,22 @@
+<article>
+	<h2 class="headline-3">Deprecation warning!</h2>
+	<p>
+		please note that <a href="https://codeberg.org/linkplay-space/kcalCalc">
+			this app code
+		</a> and this instance are both deprecated. the instance may be made unavailable
+		at any moment without further warning.
+	</p>
+</article>
+
+<style>
+	article {
+		max-width: 70ch;
+		text-wrap: balance;
+		text-align: center;
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		color: var(--md-error);
+	}
+</style>
